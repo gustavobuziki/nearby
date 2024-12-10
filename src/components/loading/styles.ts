d40312edd917342148bf711e040d7e0a70e8429a
Gustavo Buziki <gbuiki@gmail.com>
2024-12-10 13:13:20 -0300

    chore: initial commit

---
+++ src/components/loading/styles.ts
@@ -0,0 +1,11 @@
+import { colors } from "@/styles/theme";
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.gray[100],
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
